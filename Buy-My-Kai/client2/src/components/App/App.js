5108a0d2f366e996fe0eb02048a39d0eff502705
Lisa <zuzatoy@gmail.com>
2018-10-26 10:53:04 +1300

    prepare for work Profile,Eater, Grower components

--- Buy-My-Kai/client2/src/components/App/App.js
+++ Buy-My-Kai/client2/src/components/App/App.js
@@ -3,6 +3,7 @@ import { Route } from 'react-router-dom';
 import Home from '../Home/Home';
 import Login from '../Login/Login';
 import Register from '../Register/Register';
+import Profile from '../Profile/Profile';
 
 export default class App extends Component {
   render() {
@@ -11,6 +12,7 @@ export default class App extends Component {
         <Route exact path="/" component={Home} />
         <Route path="/register" component={Register} />
         <Route path="/login" component={Login} />
+        <Route path="/profile" component={Profile} />
       </div>
     );
   }
---
+++ Buy-My-Kai/client2/src/components/Profile/Profile.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {Link} from 'react-router-dom'
+
+import Grower from './Grower'
+import Eater from './Eater'
